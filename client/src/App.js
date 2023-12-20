bbf06497ea1da1e00261584a6e501fbe94dc863b
Shubhkr07 <shubh37kumar@gmail.com>
2023-12-20 21:18:36 +0530

    Updated frontend components for improved user interface and responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,28 @@
-import {BrowserRouter,Navigate,Routes,Route} from "react-router-dom";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Homepage from "scenes/homePage";
 import Loginpage from "scenes/loginPage";
 import Profilepage from "scenes/profilePage";
 import { useMemo } from "react";
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { themeSettings } from "./theme";
-import {CssBaseline,ThemeProvider} from "@mui/material";
-import {createTheme} from "@mui/material/styles";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 function App() {
   const mode = useSelector((state) => state.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)),[mode]);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
   return (
     <div className="app">
-        <BrowserRouter>
-          <ThemeProvider theme={theme}>
-            <CssBaseline/>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
           <Routes>
-            <Route path = "/" element = {<Loginpage></Loginpage>}></Route>
-            <Route path = "/home" element = {<Homepage></Homepage>}></Route>
-            <Route path = "/profile/:userId" element = {<Profilepage></Profilepage>}></Route>
+            <Route path="/" element={<Loginpage></Loginpage>}></Route>
+            <Route path="/home" element={isAuth ? <Profilepage /> : <Navigate to="/" />}></Route>
+            <Route path="/profile/:userId" element={isAuth ? <Homepage /> : <Navigate to="/" />}></Route>
           </Routes>
-          </ThemeProvider>
-        </BrowserRouter>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,73 +1,71 @@
 import express from "express";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
-import bodyParser from "body-parser";
+import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import morgan from "morgan"; 
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
-import {createPost} from "./controllers/posts.js";
+import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
-import {users,posts} from "./data/index.js";
+import { users, posts } from "./data/index.js";
 
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
+// Configurations
+const allowedHosts = [
+    'http://localhost:3000',
+]
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
+app.use(cors(
+    { origin: allowedHosts }
+));
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
-app.use(morgan( "common"));
-app.use(bodyParser.json({limit : "30mb", extended: true }));
-app.use(bodyParser.urlencoded({limit : "30mb", extended: true}));
-app.use(cors());
-app.use("/assets", express.static(path.join(dirname,'public/assets')));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use("/assets", express.static(path.join(__dirname, 'public/assets'))); //sets directory where assets are kept
 
+//  file storage setup
 const storage = multer.diskStorage({
-    destination: function(req,file,cb){
+    destination: function (req, file, cb) {  //file saved at public/assets
         cb(null, "public/assets");
     },
-    filename: function(req,file,cb){
-        cb(null,file.originalname);
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
     }
-});
-const upload = multer({storage});
+})
 
+const upload = multer({ storage });
 
-// Routes with files
-app.post("/auth/register",upload.single("picture"),register);
-app.post("/posts",verifyToken,upload.single("picture"),createPost);
+// routes with files
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost)
 
-// ROUTES
-app.use("/auth", authRoutes);
-app.use("/users",userRoutes);
-app.use("/posts",postRoutes);
+// route
+app.use("/auth", authRoutes)
+app.use("/users", userRoutes)
+app.use("/posts", postRoutes)
 
-const PORT = process.env.PORT ;
-// console.log(PORT);
+// mongoose setup
+const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
-    app.listen(PORT,() => console.log(`Server Port: ${PORT}`));
-    // Add One Time
-    // User.insertMany(users);
-    // Post.insertMany(posts);
-
-})
-.catch((error) =>console.log(`${error}`));
-
-
-
-
-
-// ZS7A38zE3KmtPSA0
-// mongodb+srv://shubh07:<password>@cluster0.e2be61j.mongodb.net/?retryWrites=true&w=majority
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    // for adding dummy information
+    // User.insertMany(users)
+    // Post.insertMany(posts)
+}).catch((error) => console.log(` ${error} did not connect`)); 
